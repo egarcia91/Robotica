a4d57948974c468ba4edc84faf7cf42c688b5a14
egarcia91 <garciaezequiel91@gmail.com>
2019-09-26 18:48:55 -0300

    se arregla el problema inverso y ademas los testeos, ojo queda pendiente el modo degree en testeos

--- EulerInverso.m
+++ EulerInverso.m
@@ -9,34 +9,31 @@ function [ angulos ] = EulerInverso( params, degrad, phi_inicial )
 		phi_inicial = 0;
 	end
 
+	tolerancia = 1e-6;
 	%%Parametros basicos
 	%%
 	%% tengo que dar libertad de que entren con una matrix o un objeto con matriz e indice
 	[mat, indice] = ParseParams(params);
 	%% se terminaron los parametros de entrada
 
-	if(mat(2,3) == 0 && mat(1,3) == 0)
+	if((abs(mat(2,3)) < tolerancia) && (abs(mat(1,3)) < tolerancia))
 
-		sPsi = -SinCos(phi_inicial, 'sin', degrad)*mat(1,1) + SinCos(phi_inicial, 'cos', degrad)*mat(2,1);
-		cPsi = -SinCos(phi_inicial, 'sin', degrad)*mat(1,2) + SinCos(phi_inicial, 'cos', degrad)*mat(2,2);
-		psi = DespejeAngulo(sPsi, cPsi, degrad);
-
-		angulos = [phi_inicial, 0, psi];
+		phi = phi_inicial;
 
 	else
 
-		cTheta = mat(3,3);
-		sTheta = sqrt(mat(2,3)*mat(2,3)+mat(1,3)*mat(1,3))*indice;
-		theta = DespejeAngulo(sTheta, cTheta, degrad);
-
-		cPhi = 0;
-		sPsi = 0;
-
 		phi = DespejeAngulo(indice*mat(2,3),indice*mat(1,3), degrad);
 
-		psi = DespejeAngulo(indice*mat(3,2), indice*-mat(3,1), degrad);
-
-		angulos = [phi, theta, psi];
 	end
+
+	sPsi = -SinCos(phi, 'sin', degrad)*mat(1,1) + SinCos(phi, 'cos', degrad)*mat(2,1);
+	cPsi = -SinCos(phi, 'sin', degrad)*mat(1,2) + SinCos(phi, 'cos', degrad)*mat(2,2);
+	psi = DespejeAngulo(sPsi, cPsi, degrad);
+
+	cTheta = mat(3,3);
+	sTheta = SinCos(phi, 'cos', degrad)*mat(1,3) + SinCos(phi, 'sin', degrad)*mat(2,3);
+	theta = DespejeAngulo(sTheta, cTheta, degrad);
+
+	angulos = [phi, theta, psi];
 
 end
--- tester.m
+++ tester.m
@@ -5,9 +5,12 @@ format long g
 
 Ntests = 16; %% va a ser este valor al cubo. OJOOO
 
-tol = 1e5; % Tolerancia tres digitos
+tol = 1e2; % Tolerancia tres digitos
+
+passingTol = 1e-2; % Tolerancia al cero
 
 degrad = 'RAD';
+%degrad = 'DEG'; %%TODO FIXME
 
 limit = 0;
 
@@ -17,7 +20,7 @@ elseif(degrad == 'DEG')
 	limit = 180;
 end
 
-phi = theta = psi = linspace(0, limit, Ntests);  %%Si esta en RAD prueba con pi, sino en DEG pruebacon angulos
+phi = theta = psi = linspace(-limit, limit, Ntests+1);  %%Si esta en RAD prueba con pi, sino en DEG pruebacon angulos
 
 tests = 0;
 passingtests = 0;
@@ -31,17 +34,66 @@ for i = 1:(Ntests)
 
 			angulosIniciales = [phi(i), theta(j), psi(k)];
 			angulosFinales = EulerInverso(EulerDirecto(angulosIniciales, degrad), degrad);
+
 			if(isequal(fix(angulosIniciales*tol)/tol, fix(angulosFinales*tol)/tol))
 				passingtests++;
 			else
-				donkeyCorner(end+1) = [angulosIniciales; angulosFinales];
+				if(abs(angulosIniciales(2)) < passingTol )
+					sumaIniciales = abs(angulosIniciales(1)+angulosIniciales(3));
+					sumaFinales = abs(angulosFinales(1)+angulosFinales(3));
+					if(abs(sumaIniciales - sumaFinales) < passingTol )
+						passingtests++;
+					else
+						if(sumaIniciales/limit > 1)
+							if(abs((2 - sumaIniciales/limit) - (sumaFinales/limit)) < passingTol )
+								passingtests++;
+							else
+								donkeyCorner(end+1) = [angulosIniciales; angulosFinales];
+							end
+						end
+					end
+				elseif(isequal(fix(angulosIniciales(2)/limit*tol)/tol,-1) && isequal(fix(angulosFinales(2)/limit*tol)/tol,1))
+					sumaIniciales = abs(angulosIniciales(1)-angulosIniciales(3));
+					sumaFinales = abs(angulosFinales(1)+angulosFinales(3));
+					if(abs(sumaIniciales - sumaFinales) < passingTol )
+						passingtests++;
+					else
+						if(sumaIniciales/limit > 1)
+							if(abs((2 - sumaIniciales/limit) - (sumaFinales/limit)) < passingTol )
+								passingtests++;
+							else
+								donkeyCorner(end+1) = [angulosIniciales; angulosFinales];
+							end
+						else
+							donkeyCorner(end+1) = [angulosIniciales; angulosFinales];
+						end
+					end
+				elseif(isequal(fix(angulosIniciales(2)/limit*tol)/tol,-1) && isequal(fix(angulosFinales(2)/limit*tol)/tol,-1))
+					sumaIniciales = abs(angulosIniciales(1)-angulosIniciales(3));
+					sumaFinales = abs(angulosFinales(1)-angulosFinales(3));
+					if(abs(sumaIniciales - sumaFinales) < passingTol )
+						passingtests++;
+					else
+						if(sumaIniciales/limit > 1)
+							if(abs((2 - sumaIniciales/limit) - (sumaFinales/limit)) < passingTol )
+								passingtests++;
+							else
+								donkeyCorner(end+1) = [angulosIniciales; angulosFinales];
+							end
+						else
+							donkeyCorner(end+1) = [angulosIniciales; angulosFinales];
+						end
+					end
+				else
+					donkeyCorner(end+1) = [angulosIniciales; angulosFinales];
+				end
 			end
 
 		end
 	end
 end
 
-printf('||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n');
+printf('||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n');
 printf('||\t\t\tResultados!\t\t\t||\n');
 printf('||\tCantidad total de testeos realizados:\t\t||\n');
 printf('||\t\t\t\t\t %i\t\t||\n', tests);
@@ -49,7 +101,7 @@ printf('||\tCantidad total de testeos pasados:\t\t||\n');
 printf('||\t\t\t\t\t %i\t\t||\n', passingtests);
 printf('||\tPorcentaje de acierto:\t\t\t\t||\n');
 printf('||\t\t\t\t\t %i%%\t\t||\n', round((passingtests/tests)*100));
-printf('||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n');
+printf('||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n');
 printf('\n\n');
 
 showDC = input('Queres ver el Donkey Corner? [yes/no]','s');
