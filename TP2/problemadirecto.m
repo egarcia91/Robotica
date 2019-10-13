045d34a4f74b3134b9e17e58f2d749365e721493
elbio9garcete <56207690+elbio9garcete@users.noreply.github.com>
2019-10-13 17:07:17 -0300

    Update problemadirecto.m

---
+++ TP2/problemadirecto.m
@@ -0,0 +1,27 @@
+clear all;
+close all;
+clc;
+
+%Condiciones iniciales
+alfa1=pi/2;
+alfa2=0;
+alfa3=0;
+alfa4=pi/2;
+alfa5=-pi/2;
+alfa6=0;
+
+%Dimensiones UR5
+a2=425;
+a3=392;
+d1=89;
+d4=93/2;
+d5=93;
+d6=82;
+
+%Para que el robot quede en posición de la Figura 1 del TP2 con ABB
+theta_1=0;
+theta_2=-pi/2;
+theta_3=pi/2;
+theta_4=pi/2;
+theta_5=-pi/2;
+theta_6=0;
