ed55ea718fa1a03432a19f43424be2bc13ce0f91
elbio9garcete <56207690+elbio9garcete@users.noreply.github.com>
2019-11-13 01:40:59 -0300

    Update anguloQ6.m
    Emprolijo el cod con numerador y denominador.
    Volví a poner el menos del orto. No me dio problemas con las pruebas hasta el momento.

--- TP2/programa/anguloQ6.m
+++ TP2/programa/anguloQ6.m
@@ -7,9 +7,10 @@ function [ angulo ] = anguloQ6( q1, q5, o, n )
 		disp('que hacemo');
 	else
 		%El menos esta mal en el paper del orttooooo
-		y = (o(1)*sin(q1)-o(2)*cos(q1))/sin(q5);
+		numerador = (o(1)*sin(q1)-o(2)*cos(q1))/sin(q5);
+    		denominador = (n(1)*sin(q1)-n(2)*cos(q1))/sin(q5);
 
-		angulo = atan2(y,(n(1)*sin(q1)-n(2)*cos(q1))/sin(q5));
+		angulo = atan2(-numerador,denominador); %%%%VOLVI A PONER EL SIGNO MENOS para ver si afectaba
 	end
 	%%Ya tenemos q6!!!!!!
 	angulo = intervaloAngulo( angulo );
