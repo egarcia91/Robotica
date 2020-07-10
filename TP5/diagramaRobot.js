a8a501a88fa2e3f08db06989234f24ce38dad0f7
egarcia91 <garciaezequiel91@gmail.com>
2020-07-10 00:45:48 -0300

    se agrega logica del Scara

--- TP5/diagramaRobot.js
+++ TP5/diagramaRobot.js
@@ -31,5 +31,3 @@
 
 	window.DiagramaRobot = DiagramaRobot;
 })();
-
-
--- TP5/generadorTrayectoria.js
+++ TP5/generadorTrayectoria.js
@@ -9,11 +9,41 @@
 
 	GeneradorTrayectoria.prototype.constructor = "GeneradorTrayectoria";
 	GeneradorTrayectoria.prototype.cantidadEquiespacios = 1001;
+	GeneradorTrayectoria.prototype.largoEslabon1 = 0.4;
+	GeneradorTrayectoria.prototype.largoEslabon2 = 0.3;
 
 	GeneradorTrayectoria.prototype.lista = [
 		"Ideal",
 		"Real"
 	];
+
+	GeneradorTrayectoria.prototype.transformacion = function(x, y, z, g){
+		// pose = [x,y,z,g]'; siendo g el indice de configuracion.
+
+		// Parametros del doble pendulo
+		var a1 = this.largoEslabon1; //[m]
+		var a2 = this.largoEslabon2; //[m]
+
+		var c2 = (x*x + y*y -a1*a1 -a2*a2)/(2*a1*a2);
+
+		var t1 = undefined;
+		var t2 = undefined;
+
+		if( math.abs(c2) <= 1){
+			var s2 = g*math.sqrt(1-c2*c2);
+			t2 = math.atan2(s2,c2);
+
+			var factor = (a2*s2)/(a1 + a2*c2);
+			var s1 = (y - x*factor)/((a1 + a2*c2) - factor*(a2*s2));
+			var c1 = (x/(a1 + a2*c2)) + s1*factor;
+			t1 = math.atan2(s1,c1);
+		}
+
+		return {
+			't1' : t1,
+			't2' : t2
+		};
+	};
 
 	GeneradorTrayectoria.prototype.generar = function(data){
 
@@ -29,5 +59,3 @@
 
 	window.GeneradorTrayectoria = GeneradorTrayectoria;
 })();
-
-
